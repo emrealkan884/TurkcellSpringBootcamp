a0ec62441df9defa2d431a812c49ea47ed268739
emrealkan884 <Hasan.alkan884@gmail.com>
2023-10-10 22:21:10 +0300

    Multi Language

---
+++ src/main/java/com/turkcell/spring/controllers/HomeController.java
@@ -0,0 +1,21 @@
+package com.turkcell.spring.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("home")
+@RequiredArgsConstructor
+public class HomeController {
+
+    private final MessageSource messageSource;
+
+    @GetMapping("")
+    public String get(){
+        return messageSource.getMessage("hello", null, LocaleContextHolder.getLocale());
+    }
+}
